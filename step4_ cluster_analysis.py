90f9dbe506e81b607caaee1781383fe7d2452b15
Alberto Larraz <151042834+alberlarraz@users.noreply.github.com>
2024-04-24 17:03:10 +0200

    Update step4_ cluster_analysis.py

--- step4_ cluster_analysis.py
+++ step4_ cluster_analysis.py
@@ -1,4 +1,4 @@
-#STEP 1: IMPORT LIBRARIES 
+#IMPORTAR LIBRERIAS: PANDAS, NUMPY, SCIPY, PYLAB Y MATPLOTLIB
 
 import pandas as pd
 import numpy as np
@@ -9,27 +9,23 @@ from scipy.cluster.vq import whiten
 from scipy.spatial.distance import pdist, squareform
 from matplotlib import pyplot as plt
 
-#STEP 2: UPLOAD DATA FROM AN EXCEL FILE
-
+#IMPORTAR DATOS DESDE UN ARCHIVO EXCEL
+#El archivo excel será el mismo que se ha usado como input en el STEP 2, pero con las columnas de los endpoints eliminados borradas.
 pd.options.display.float_format = "{:,.1f}".format
 data_alergenos = pd.read_excel("descriptores_posproc.xlsx")
-data_alergenos.index = data_alergenos["COMP"]
+data_alergenos.index = data_alergenos["COMP"] #O el título de la cabecera elegida para la columna de las moléculas
+#Nombre de los descriptores escogidos
 data_alergenos_numeric = data_alergenos[["BCUT.4", "BCUT.2", "petitjeanShapeIndex.1", "CPSA.21", "WHIM.3", "MaxEStateIndex", "Weta2.unity", "WNSA-1", "Wlambda2.unity",
  "types.GeometricalRadius", "WD.unity", "ALogP", "ALogp2", "ATSC3c", "ATSC1e", "ATSC3i", "AATSC3m", "AATSC2i", "MATS2m", "MATS1s", "MATS3s", "GATS1c", "GATS2c", "GATS3e",
   "VE3_Dzp", "VR3_Dzi", "SM1_Dzs", "VR1_Dzs", "SpMin5_Bhs", "AVP-1", "gmin", "BIC3", "TDB3m", "TDB3i", "WPSA-326", "RNCS33", "GRAV-444", "RDF35u", "RDF15s", "E1m", "Dv", "De",
    "E2p", "E2i", "E1s", "Ks"
 ]].astype(float)
 
-#STEP 3: USE OF WHITEN FUNCTION TO HAVE THE SAME STANDARD DESVIATION FOR EACH COLUMN -> Equal influence
-
+#USO DE LA FUNCIÓN WHITEN (BLANQUEO) PARA TENER LA MISMA DESVIACIÓN ESTANDAR POR CADA COLUMNA (IGUAL INFLUENCIA)
 whiten_numeric = pd.DataFrame(data = whiten(obs = data_alergenos_numeric, check_finite = True), columns=data_alergenos_numeric.columns)
 whiten_numeric.index = data_alergenos_numeric.index
-#print(whiten_numeric)
-#for j in whiten_numeric.columns:
-#   print([j, whiten_numeric[j].std()])
 
-#STEP 4.1: DISTANCE METRIC (Distance matrix with Euclidian distance)
-
+#DISTANCE METRIC (Recomendación: Matriz de distancias con la distancia Euclidea)
 dict_pdist = {}
 list_metric = [
     "euclidean", 
@@ -45,7 +41,7 @@ for j in list_metric:
 #print(dict_pdist)
 #print(squareform(X=dict_pdist["euclidean"])) # -> To see how the whole matrix looks like
 
-#STEP 4.2: DISTANCE METRIC (Method Linkage: Complete)
+#DISTANCE METRIC (Recomendación del método de enlace: Complete)
 dict_linkage = {}
 list_method = [
     "single",
@@ -66,8 +62,7 @@ for j in list_metric:
         )
 #print(dict_linkage["euclidean_complete"])
 
-#STEP 5: PLOT THE DENDROGRAM
-
+#REPRESENTAR EL DENDOGRAMA (sirve para la identificación de outliers, y selección del test set y training set de forma homogénea)
 fig = plt.figure(figsize=(18,15))
 dendrogram(
     Z = dict_linkage['euclidean_complete'],
@@ -80,7 +75,7 @@ dendrogram(
 
 plt.show()
 
-#STEP 6: SAVE THE DATA
+#GUARDAR LOS DATOS
 threshold= input("Threshold: ")
 clusters = fcluster(Z=dict_linkage['euclidean_complete'], t=float(threshold), criterion='distance')
 data_alergenos_numeric['Cluster'] = clusters
