3cb3f659c50b46532a7cc22680dbb02903d84a4a
Alberto Larraz <151042834+alberlarraz@users.noreply.github.com>
2024-04-24 15:35:05 +0200

    Update step2_correlation_matrix.py

--- step2_correlation_matrix.py
+++ step2_correlation_matrix.py
@@ -1,12 +1,16 @@
+#IMPORTAR PANDAS COMO LIBRERÍA
 import pandas as pd
 
-# Importamos dataframe
+#IMPORTAR EXCEL CON LOS DESCRIPTORES
+#Respecto al excel obtenido en el anterior paso deben hacerse los siguientes cambios
+#La primera columna contendrá el nombre de las moléculas. Recomendado nombrarlas como C1, C2, C3, ..., Cn
+#El resto de columnas deben ser los resultados de todos los descriptores. La primera fila queda reservada para el nombre de los descriptores.
 dataset = pd.read_excel('descriptores_posproc.xlsx')
 df = pd.DataFrame(dataset)
 
-# Calcular la matriz de correlación
+#CÁLCULO DE LA MATRIZ DE CORRELACIÓN
 correlation_matrix = df.corr()
 
-# Imprimir la matriz de correlación
+#IMPRIMIR LA MATRIZ DE CORRELACIÓN
 correlation_matrix.to_csv("correlation_matrix_act.csv", index=False)
 print("MATRIZ DE CORRELACIÓN FINALIZADA")
